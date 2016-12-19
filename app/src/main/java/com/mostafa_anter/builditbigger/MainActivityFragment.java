c85348391283123908ff75a2f3f083ea2270cf01
mostafa_anter <ga30000@gmail.com>
2016-12-19 16:05:15 +0200

    Step 1: Create a Java library

--- app/src/main/java/com/mostafa_anter/builditbigger/MainActivityFragment.java
+++ app/src/main/java/com/mostafa_anter/builditbigger/MainActivityFragment.java
@@ -5,6 +5,12 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
+
+import com.javaJokes.Joker;
+
+import butterknife.ButterKnife;
+import butterknife.OnClick;
 
 /**
  * A placeholder fragment containing a simple view.
@@ -17,6 +23,18 @@ public class MainActivityFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_main, container, false);
+        View view = inflater.inflate(R.layout.fragment_main, container, false);
+        ButterKnife.bind(this, view);
+        return view;
+    }
+
+    // R2 used inside annotation :)
+    @OnClick({R2.id.tell_joke})
+    public void doAction(View view){
+        switch (view.getId()){
+            case R.id.tell_joke:
+                Toast.makeText(getActivity(), new Joker().getJoke(), Toast.LENGTH_SHORT).show();
+                break;
+        }
     }
 }
---
+++ javajokes/src/main/java/com/javaJokes/Joker.java
@@ -0,0 +1,7 @@
+package com.javaJokes;
+
+public class Joker {
+    public String getJoke() {
+        return "This is totally a funny joke";
+    }
+}
