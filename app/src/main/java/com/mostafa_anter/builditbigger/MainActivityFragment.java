d603c7f5f940bebf16eb7d87059dcac5d9013b6c
mostafa_anter <ga30000@gmail.com>
2016-12-20 01:20:14 +0200

    Step 3: Create GCE Module

--- app/src/main/java/com/mostafa_anter/builditbigger/MainActivityFragment.java
+++ app/src/main/java/com/mostafa_anter/builditbigger/MainActivityFragment.java
@@ -1,16 +1,31 @@
 package com.mostafa_anter.builditbigger;
 
+import android.content.Context;
 import android.content.Intent;
-import android.support.v4.app.Fragment;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.support.v4.util.Pair;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ProgressBar;
 import android.widget.Toast;
 
+import com.google.api.client.extensions.android.http.AndroidHttp;
+import com.google.api.client.extensions.android.json.AndroidJsonFactory;
+import com.google.api.client.googleapis.services.AbstractGoogleClientRequest;
+import com.google.api.client.googleapis.services.GoogleClientRequestInitializer;
 import com.javaJokes.Joker;
+import com.mostafa_anter.backend.myApi.MyApi;
 import com.mostafa_anter.displayJoke.DisplayJokeActivity;
 
+import java.io.IOException;
+
+import butterknife.BindView;
 import butterknife.ButterKnife;
 import butterknife.OnClick;
 
@@ -18,6 +33,8 @@ import butterknife.OnClick;
  * A placeholder fragment containing a simple view.
  */
 public class MainActivityFragment extends Fragment {
+
+    @BindView(R2.id.progressBar)ProgressBar progressBar;
 
     public MainActivityFragment() {
     }
@@ -30,18 +47,90 @@ public class MainActivityFragment extends Fragment {
         return view;
     }
 
+    @Override
+    public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        progressBar.setVisibility(View.GONE);
+    }
+
     // R2 used inside annotation :)
     @OnClick({R2.id.tell_joke})
     public void doAction(View view){
         switch (view.getId()){
             case R.id.tell_joke:
-                // call java jokes library
-                String joke = new Joker().getJoke();
-                // call display joke android library
-                Intent intent = new Intent(getActivity(), DisplayJokeActivity.class);
-                intent.putExtra("joke", joke);
-                startActivity(intent);
+                if (isOnline(getActivity())) {
+                    // call java jokes library
+                    String joke = new Joker().getJoke();
+                    // pass joke to GCE to retrieve it again from GCE :)
+                    new EndpointsAsyncTask().execute(new Pair<Context, String>(getActivity(), joke));
+                } else {
+                    Toast.makeText(getActivity(), R.string.there_is_no_network, Toast.LENGTH_LONG).show();
+                }
                 break;
+        }
+    }
+
+    class EndpointsAsyncTask extends AsyncTask<Pair<Context, String>, Void, String> {
+        private MyApi myApiService = null;
+        private Context context;
+
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+            progressBar.setVisibility(View.VISIBLE);
+        }
+
+        @Override
+        protected String doInBackground(Pair<Context, String>... params) {
+            if(myApiService == null) {  // Only do this once
+                MyApi.Builder builder = new MyApi.Builder(AndroidHttp.newCompatibleTransport(),
+                        new AndroidJsonFactory(), null)
+                        // options for running against local devappserver
+                        // - 10.0.2.2 is localhost's IP address in Android emulator
+                        // - turn off compression when running against local devappserver
+                        .setRootUrl("https://build-it-bigger-153020.appspot.com/_ah/api/")
+                        .setGoogleClientRequestInitializer(new GoogleClientRequestInitializer() {
+                            @Override
+                            public void initialize(AbstractGoogleClientRequest<?> abstractGoogleClientRequest) throws IOException {
+                                abstractGoogleClientRequest.setDisableGZipContent(true);
+                            }
+                        });
+                // end options for devappserver
+
+                myApiService = builder.build();
+            }
+
+            context = params[0].first;
+            String joke = params[0].second;
+
+            try {
+                return myApiService.getJoke(joke).execute().getData();
+            } catch (IOException e) {
+                return e.getMessage();
+            }
+        }
+
+        @Override
+        protected void onPostExecute(String result) {
+            progressBar.setVisibility(View.GONE);
+            Intent intent = new Intent(getActivity(), DisplayJokeActivity.class);
+            intent.putExtra("joke", result);
+            startActivity(intent);
+        }
+    }
+
+    /**
+     *
+     * @param mContext
+     * @return state of network state
+     */
+    private boolean isOnline(Context mContext) {
+        ConnectivityManager cm = (ConnectivityManager) mContext.getSystemService(Context.CONNECTIVITY_SERVICE);
+        NetworkInfo netInfo = cm.getActiveNetworkInfo();
+        if (netInfo != null && netInfo.isConnectedOrConnecting()) {
+            return true;
+        } else {
+            return false;
         }
     }
 }
