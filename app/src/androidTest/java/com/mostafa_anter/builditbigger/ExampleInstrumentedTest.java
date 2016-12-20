d10ba62c97ca00a8bf6266312fd93cf906819428
mostafa_anter <ga30000@gmail.com>
2016-12-20 02:19:55 +0200

    Step 4: Add Functional Tests

--- app/src/androidTest/java/com/mostafa_anter/builditbigger/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/com/mostafa_anter/builditbigger/ExampleInstrumentedTest.java
@@ -1,11 +1,22 @@
 package com.mostafa_anter.builditbigger;
 
 import android.content.Context;
+import android.os.AsyncTask;
 import android.support.test.InstrumentationRegistry;
 import android.support.test.runner.AndroidJUnit4;
+import android.support.v4.util.Pair;
+
+import com.google.api.client.extensions.android.http.AndroidHttp;
+import com.google.api.client.extensions.android.json.AndroidJsonFactory;
+import com.google.api.client.googleapis.services.AbstractGoogleClientRequest;
+import com.google.api.client.googleapis.services.GoogleClientRequestInitializer;
+import com.javaJokes.Joker;
+import com.mostafa_anter.backend.myApi.MyApi;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
+
+import java.io.IOException;
 
 import static org.junit.Assert.*;
 
@@ -22,5 +33,40 @@ public class ExampleInstrumentedTest {
         Context appContext = InstrumentationRegistry.getTargetContext();
 
         assertEquals("com.mostafa_anter.builditbigger", appContext.getPackageName());
+    }
+
+    @Test
+    public void checkNonEmptyString() throws Exception{
+        new AsyncTask<String, Void, String>(){
+            private MyApi myApiService = null;
+            @Override
+            protected String doInBackground(String... strings) {
+                MyApi.Builder builder = new MyApi.Builder(AndroidHttp.newCompatibleTransport(),
+                        new AndroidJsonFactory(), null)
+                        .setRootUrl("https://build-it-bigger-153020.appspot.com/_ah/api/")
+                        .setGoogleClientRequestInitializer(new GoogleClientRequestInitializer() {
+                            @Override
+                            public void initialize(AbstractGoogleClientRequest<?> abstractGoogleClientRequest) throws IOException {
+                                abstractGoogleClientRequest.setDisableGZipContent(true);
+                            }
+                        });
+                myApiService = builder.build();
+                String joke = strings[0];
+
+                try {
+                    return myApiService.getJoke(joke).execute().getData();
+                } catch (IOException e) {
+                    return e.getMessage();
+                }
+            }
+
+            @Override
+            protected void onPostExecute(String s) {
+                super.onPostExecute(s);
+                assertNotNull(s);
+                assertTrue(s.length() > 0);
+            }
+        }.execute(new Joker().getJoke());
+
     }
 }
